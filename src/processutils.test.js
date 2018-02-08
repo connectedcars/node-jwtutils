4517d7da207ca10163af4b7fa943d0d97a005947
Troels Liebe Bentsen <tlb@nversion.dk>
2018-02-08 17:17:19 +0100

    Make own sleep so it gives the same output on mac and linux

--- src/processutils.test.js
+++ src/processutils.test.js
@@ -1,8 +1,31 @@
 const expect = require('unexpected')
 
 const ProcessUtils = require('./processutils')
+const tmp = require('tmp')
+const path = require('path')
+const fs = require('fs')
 
 describe('ProcessUtils', () => {
+  let tmpdir
+  let oldPath
+  before(() => {
+    tmpdir = tmp.dirSync()
+    oldPath = process.env['PATH']
+    process.env['PATH'] = `${tmpdir.name}${path.delimiter}${oldPath}`
+    fs.writeFileSync(
+      `${tmpdir.name}/sleep`,
+      `#!${
+        process.argv[0]
+      }\nsetTimeout(()=> {}, process.argv[1] * 1000)\nconsole.error('Done sleeping')`
+    )
+    fs.chmodSync(`${tmpdir.name}/sleep`, '755')
+  })
+  after(() => {
+    process.env['PATH'] = oldPath
+    fs.unlinkSync(`${tmpdir.name}/sleep`)
+    tmpdir.removeCallback()
+  })
+
   it('should timeout', () => {
     let [cmd, resultPromise] = ProcessUtils.runProcessAsync('sleep', ['10'], {
       timeout: 1
@@ -10,9 +33,9 @@ describe('ProcessUtils', () => {
     return expect(resultPromise, 'to be rejected with', new Error('Timeout'))
   })
   it('should generate stderr', () => {
-    let [cmd, resultPromise] = ProcessUtils.runProcessAsync('sleep', ['--help'])
+    let [cmd, resultPromise] = ProcessUtils.runProcessAsync('sleep', [])
     return expect(resultPromise, 'to be fulfilled with', {
-      stderr: 'usage: sleep seconds\n'
+      stderr: 'Done sleeping\n'
     })
   })
 })
