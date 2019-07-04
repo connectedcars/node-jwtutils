8538d7745f4d856f6fa7ff8c112ca4c5b74ec972
mathiastj <2278040+mathiastj@users.noreply.github.com>
2019-07-04 14:22:59 +0200

    Use class syntax in JwtVerifyError

--- src/jwtverifyerror.js
+++ src/jwtverifyerror.js
@@ -1,13 +1,8 @@
-// @ts-check
-'use strict'
-
-function JwtVerifyError(message, innerError) {
-  this.name = 'JwtVerifyError'
-  this.message = message
-  this.stack = new Error().stack
-  this.innerError = innerError || null
+class JwtVerifyError extends Error {
+  constructor(message, innerError = null) {
+    super(message)
+    this.innerError = innerError
+  }
 }
-JwtVerifyError.prototype = Object.create(Error.prototype)
-JwtVerifyError.prototype.constructor = JwtVerifyError
 
 module.exports = JwtVerifyError
