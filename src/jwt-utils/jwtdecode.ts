708e604a2103df1f73731d945d87c6d4c4d19340
Grace Bullock <gb@connectedcars.dk>
2023-08-02 13:12:02 +0200

    remove unused interface

--- src/jwt-utils/jwtdecode.ts
+++ src/jwt-utils/jwtdecode.ts
@@ -11,24 +11,6 @@ const defaultOptions = {
   fixup: null
 }
 
-// export interface JwtBody {
-//   iss?: string // Issuing authority of this token, i.e. our identity provider
-//   ons?: string // Organization namespace this token is issued within
-//   sub?: string // Identifier for the party this token is issued on behalf of
-//   aud?: string | string[] // Target audience for this token, i.e. our applications
-//   acr?: string // The level of authentication, i.e. AM1
-//   jti?: string // Unique id for this token
-//   sid?: string // Unique id for this session
-//   amr?: string[] // Access methods used to obtain this token, can be a combination, i.e. password and sms otp
-//   exp?: number // Timestamp for expiry
-//   iat?: number // Timestamp for issuing date
-//   nbf?: number // Timestamp which this token should not be used before
-//   clt?: number // Current life time counting number of refresshes in this session
-//   scope?: string[]
-//   email?: string
-//   email_verified?: boolean
-// }
-
 export function decode(
   jwt: string,
   publicKeys: Record<string, Record<string, string | PublicKey>>,
